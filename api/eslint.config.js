db24eeceab7b2ba9f617e8b4caf8a371cfdacbcf
Irving Dinh <irving.dinh@gmail.com>
2026-01-18 22:38:28 +0700

    chore(api): configure ESLint with import sorting

---
+++ api/eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+        },
+      ],
+    },
+  },
+  {
+    ignores: ['node_modules/**', 'dist/**', 'bun.lock'],
+  }
+);
